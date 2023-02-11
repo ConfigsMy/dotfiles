add0c5f6d72c66c5f067497e57d136af0bb6ef36
Artur Isaev <isaev.artur.1999@mail.ru>
2023-02-11 19:28:42 +0600

    dotfiles repo init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@ require('lazy-plugin-manager')
 
 require('settings')
 
+vim.opt.clipboard = "unnamedplus"
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", {})
 vim.g.mapleader = " "
 require('mappings')
